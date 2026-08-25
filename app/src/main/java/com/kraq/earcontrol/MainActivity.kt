59838a71fd0df4cd0b17c2a5835d4577bbc8639b
kraqinc <275528312+kraqinc@users.noreply.github.com>
2026-08-25 17:14:59 -0500

    Fix duplicate imports

--- app/src/main/java/com/kraq/earcontrol/MainActivity.kt
+++ app/src/main/java/com/kraq/earcontrol/MainActivity.kt
@@ -31,7 +31,6 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
